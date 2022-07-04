88e9c638201128cc366ec6094ebe46d4712f116d
Maxime CAT <maximecat@outlook.fr>
2022-07-04 19:34:52 +0200

    Affichage éléments page principale

---
+++ public/js/controllers/homePage.js
@@ -0,0 +1,8 @@
+import { RecipeFactory } from "../factories/RecipeFactory.js";
+import { RecipeService } from "../service/RecipeService.js";
+
+class HomePageController {
+
+}
+
+new HomePageController();
