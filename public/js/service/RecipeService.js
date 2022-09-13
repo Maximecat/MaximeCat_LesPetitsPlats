4524dfacda3b1308e49fd5ad8abace08f61750c2
Maxime CAT <maximecat@outlook.fr>
2022-09-13 10:16:57 +0200

    Récupération Ingredient, Appareil et Ustensile

--- public/js/service/RecipeService.js
+++ public/js/service/RecipeService.js
@@ -9,4 +9,41 @@ export class RecipeService {
             .then(datas => datas.recipes.map(recipe => new Recipe(recipe)))
             .catch(err => console.error(err))
     }
+
+    getIngredients(recipes) {
+
+        return new Array(
+            ...new Set(
+                recipes.map(recipe => {
+                    return recipe.ingredients.map(ingredient => {
+                        return ingredient.ingredient;
+                    });
+                }).flat()
+            )
+        )
+    }
+
+    getAppareils(recipes) {
+
+        return new Array(
+            ...new Set(
+                recipes.map(recipe => {
+                    return recipe.appliance;
+                })
+            )
+        )
+    }
+
+    getUstensiles(recipes) {
+
+        return new Array(
+            ...new Set(
+                recipes.map(recipe => {
+                    return recipe.ustensils.map(ustensils => {
+                        return ustensils;
+                    });
+                }).flat()
+            )
+        )
+    }
 }
